cc930d050fb88c889590045a5e1d7ca82756b676
farpires <arpirez.fabio@gmail.com>
2023-01-12 19:05:28 -0300

    Add: page home has been added

---
+++ src/page/home/home.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Home = () => {
+  return (
+    <div className="jumbotron">
+      <p className="lead text-center">Spotify albums</p>
+    </div>
+  );
+};
+
+export default Home;
